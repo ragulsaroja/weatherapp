d98497900c3bb68ddec86eb22a119e7b8491d237
AshwinpillaiD <ragulsaroja@gmail.com>
2024-04-15 12:33:09 +0530

    base.html config

--- src/myapp/views.py
+++ src/myapp/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
-    return render(request,'weather/index.html')
+def home(request):
+    return render(request,'weather/weatherapp.html')
